a8a75e20b196b632fe99f29d0123e7bbf474a4e0
Salman Tyagi <salmantyagi15@gmail.com>
2024-09-07 18:04:04 +0530

    added: interfaces type for action in reducer function

--- src/features/reducers/repositoriesReducer.ts
+++ src/features/reducers/repositoriesReducer.ts
@@ -4,18 +4,38 @@ interface RepositoriesState {
   error: string;
 }
 
+interface RepositoriesFetchingAction {
+  type: 'repositoriesFetching';
+}
+
+interface RepositoriesFetchedAction {
+  type: 'repositoriesFetched';
+  payload: string[];
+}
+
+interface RepositoriesErrorAction {
+  type: 'repositoriesError';
+  error: string;
+}
+
 const initialState: RepositoriesState = {
   isLoading: false,
   data: [],
   error: ''
 };
 
-const repositoriesReducer = (state = initialState, action: any) => {
+const repositoriesReducer = (
+  state = initialState,
+  action:
+    | RepositoriesFetchingAction
+    | RepositoriesFetchedAction
+    | RepositoriesErrorAction
+): RepositoriesState => {
   switch (action.type) {
-    case 'repositoriesFetchRepositories':
+    case 'repositoriesFetching':
       return { ...state, isLoading: true };
 
-    case 'repositoriesFetchedRepositories':
+    case 'repositoriesFetched':
       return { ...state, isLoading: false, data: action.payload };
 
     case 'repositoriesError':
